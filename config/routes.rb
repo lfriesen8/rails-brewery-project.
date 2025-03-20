a0969cf103122a8b892c6d539aa20247b874270f
Liam Friesen <lfriesen8@rrc.ca>
2025-03-20 00:10:21 -0500

    nav bar designed, and design structering

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "breweries#index"
+  get "/about", to: "pages#about"  # Ensure the About page is accessible
+
+  root "breweries#index"  # Keep breweries as the homepage
   resources :breweries, only: [:index, :show]
   resources :beers, only: [:index, :show]
 end
