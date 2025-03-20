241815b7a3f0d12c1255a22dbfe0fc0abda9bbea
Liam Friesen <lfriesen8@rrc.ca>
2025-03-20 02:44:34 -0500

    creating search function, bit buggy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/pubs", to: "breweries#pubinfo"  # New route for pubs page
+  
   root "breweries#index"  # Home page remains the same
+  
   resources :breweries, only: [:index, :show]  
   resources :beers, only: [:index, :show]
+
+  # DONT FORGET TO ADD THIS: Route for Search Functionality
+  get "/search", to: "search#results"
 end
